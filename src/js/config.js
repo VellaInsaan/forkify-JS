3f2e8c0bf7c0f0b0d25474c7007b7654e0e286e4
Satyam <satus2311@gmail.com>
2022-08-26 13:44:21 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dad7a423-c05b-49e4-8e59-2a39e217009a';
+export const MODAL_CLOSE_SEC = 2.5;
